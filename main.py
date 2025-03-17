f5fa465aa3cdf71c8dec10fe18205f68b8c6a5d6
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2025-03-17 11:11:44 -0300

    cog and change main

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import requests
+import secrets_env
+import os
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# @bot.command(aliases=['pipin',"pepen"])
+# async def ping(ctx, *args):
+#     latency = round(bot.latency * 1000)
+#     await ctx.send(f"Pong! Latencia del bot: {latency} ms.")
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            print(filename)
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(secrets_env.TOKEN)
+
+asyncio.run(main())
